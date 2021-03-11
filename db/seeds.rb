7f6044c6fb8ee9fe63cd1fd6c9c326a3f6d27f77
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-11 23:34:28 +0100

    add seed + first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+john = User.create!(
+  email: "john.doe@example.com",
+  password: "password"
+)
