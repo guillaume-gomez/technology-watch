8716eccbea44a684c3ff6b86de50f423c7e01a4e
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-03-13 19:16:22 +0100

    add create user mutation

---
+++ app/graphql/mutations/user_mutations.rb
@@ -0,0 +1,39 @@
+module Mutations
+  module UserMutations
+    # Defining Type used as argument to create User entries
+    class UserInputType < Types::BaseInputObject
+      graphql_name 'UserInput'
+      description 'Properties for creating a User'
+
+      argument :email, String, required: true do
+        description 'User email'
+      end
+
+      argument :password, String, required: true do
+        description 'User password'
+      end
+
+      argument :first_name, String, required: true do
+        description 'First name'
+      end
+
+      argument :last_name, String, required: true do
+        description 'Last name'
+      end
+    end
+
+    class CreateUser < BaseMutation
+      description 'Create a User'
+      type Types::UserType
+
+      argument :user, UserInputType, required: true
+
+      def resolve(user:)
+        record = User.new(user.to_h)
+        record.save!
+        record
+      end
+    end
+  end
+end
+
