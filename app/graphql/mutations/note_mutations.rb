3d935e34be6992242a0b7ac4831021fb4ea811d8
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-04-02 01:32:42 +0200

    add note edition + note deletion

--- app/graphql/mutations/note_mutations.rb
+++ app/graphql/mutations/note_mutations.rb
@@ -29,6 +29,39 @@ module Mutations
       end
     end
 
+    class NoteEditType < Types::BaseInputObject
+      graphql_name 'NoteEditType'
+      description 'Properties for editing a Note'
+      
+      argument :id, ID, required: true do
+        description 'Note id'
+      end
+
+      argument :name, String, required: false do
+        description 'Note name'
+      end
+
+      argument :link, String, required: false do
+        description 'Note link'
+      end
+
+      argument :user_id, String, required: false do
+        description 'Author id of the note'
+      end
+
+      argument :description, String, required: false do
+        description 'Note description'
+      end
+      
+      argument :rating, Integer, required: false do
+        description 'Note rating'
+      end
+
+      argument :time_to_read, GraphQL::Types::ISO8601DateTime, required: false do
+        description 'Time to read the note'
+      end
+    end
+
     class CreateNote < BaseMutation
       description 'Create a Note'
       type Types::NoteType
@@ -38,6 +71,36 @@ module Mutations
       def resolve(note:)
         record = Note.new(note.to_h)
         record.save!
+        record
+      end
+    end
+
+    class EditNote < BaseMutation
+      description 'Edit a Note'
+      type Types::NoteType
+
+      argument :note, NoteEditType, required: true
+
+      def resolve(note:)
+        record = Note.find(note[:id])
+        if record.user_id == context[:current_resource].id
+          record.assign_attributes(note.to_h)
+          record.save!
+        end
+        record
+      end
+    end
+
+    class DestroyNote < BaseMutation
+      description 'Destroy a Note'
+      type Types::NoteType
+
+      argument :id, ID, required: true
+
+      def resolve(id:)
+        record = context[:current_resource].notes.find(id)
+        debugger
+        record.destroy
         record
       end
     end
