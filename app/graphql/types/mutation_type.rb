9ae9f1671fe283344a155bf1d9e7796bdf6aa235
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-04-02 20:35:15 +0200

    update graphql schema

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,6 +8,8 @@ module Types
     end
 
     field :create_user, resolver: Mutations::UserMutations::CreateUser, null: false, authenticate: true
-    field :create_note, resolver: Mutations::NoteMutations::CreateNote, null: false, authenticate: false
+    field :create_note, resolver: Mutations::NoteMutations::CreateNote, null: false, authenticate: true
+    field :edit_note, resolver: Mutations::NoteMutations::EditNote, null: false, authenticate: true
+    field :destroy_note, resolver: Mutations::NoteMutations::DestroyNote, null: false, authenticate: true
   end
 end
