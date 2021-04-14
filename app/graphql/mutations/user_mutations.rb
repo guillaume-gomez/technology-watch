d5dc397cf8cccb54023ee5ad42daa538545cc225
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-04-15 00:03:43 +0200

    update user

--- app/graphql/mutations/user_mutations.rb
+++ app/graphql/mutations/user_mutations.rb
@@ -1,6 +1,5 @@
 module Mutations
   module UserMutations
-    # Defining Type used as argument to create User entries
     class UserInputType < Types::BaseInputObject
       graphql_name 'UserInput'
       description 'Properties for creating a User'
@@ -22,6 +21,33 @@ module Mutations
       end
     end
 
+    class UserEditType < Types::BaseInputObject
+      graphql_name 'UserEditType'
+      description 'Properties for editing a User'
+
+      argument :id, ID, required: true do
+        description 'user id'
+      end
+
+      argument :name, String, required: false do
+        description 'full name'
+      end
+
+      argument :nickname, String, required: false do
+        description 'nickname'
+      end
+
+      argument :language, String, required: false do
+        description 'application language'
+      end
+
+      argument :tags, [String], required: false, as: :tag_list do
+        description 'list of suggested tags on notes'
+      end
+
+
+    end
+
     class CreateUser < BaseMutation
       description 'Create a User'
       type Types::UserType
@@ -30,6 +56,23 @@ module Mutations
 
       def resolve(user:)
         record = User.new(user.to_h)
+        record.save!
+        record
+      end
+    end
+
+    class EditUser < BaseMutation
+      description 'Edit a User'
+      type Types::UserType
+
+      argument :user, UserEditType, required: true
+
+      def resolve(user:)
+        record = User.find(user[:id])
+        if !context[:current_resource].is_super_admin && record.id != context[:current_resource].id
+          return record
+        end
+        record.assign_attributes(user.to_h)
         record.save!
         record
       end
--- spec/graphql/mutations/user_mutations_spec.rb
+++ spec/graphql/mutations/user_mutations_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Mutations::UserMutations, type: :graphql do
   include_context 'with graphql query request'
-  let(:query) do
-    <<~GQL
-      mutation($input: UserInput!) {
-        createUser(input: { user: $input }) {
-          id
-        }
-      }
-    GQL
-  end
 
-  describe "resolve" do
-    let!(:current_user) { create(:user) }
-    let(:variables) { {input: params } }
-    subject{ execute_graphql }
+  let!(:current_user) { create(:user) }
+  let(:variables) { {input: params } }
+  subject{ execute_graphql }
+  
+  describe "createUser resolver" do
+    let(:query) do
+      <<~GQL
+        mutation($input: UserInput!) {
+          createUser(input: { user: $input }) {
+            id
+          }
+        }
+      GQL
+    end
+
 
     context "when inputs are valid" do
       let(:params) do
@@ -31,4 +33,48 @@ RSpec.describe Mutations::UserMutations, type: :graphql do
       it { expect{subject}.not_to change { User.count } }
     end
   end
+
+  describe "resolve editUser" do
+    let(:query) do
+      <<~GQL
+        mutation($input: UserEditType!) {
+          editUser(input: { user: $input }) {
+            id
+          }
+        }
+      GQL
+    end
+    
+    context "when current_user change nickname, and tags" do
+      let(:params) do
+        { id: current_user.id, nickname:"Another nickname" }
+      end
+      it { expect{subject}.to change { current_user.reload.nickname } }
+    end
+
+    context "when try to change a non existing user" do
+      let(:params) { { id: -1 } }
+      it { expect{subject}.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+
+    context "when try to change another user and current_user is NOT super admin" do
+      let(:current_user) { create(:user)}
+      let(:another_user) { create(:user)}
+      let(:params) do
+        { id: another_user.id, nickname: "My first try" }
+      end
+      it { expect{subject}.not_to change { another_user.reload.nickname } }
+    end
+
+    context "when try to change another user and current_user is super admin" do
+      let(:current_user) { create(:user, :super_admin)}
+      let(:another_user) { create(:user)}
+      let(:params) do
+        { id: another_user.id, nickname: "My first try" }
+      end
+      it { expect{subject}.to change { another_user.reload.nickname } }
+    end
+  end
+
+
 end
