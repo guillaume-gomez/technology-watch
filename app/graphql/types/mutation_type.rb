7b3380ec4bac021f73a70c76e858b2e67be1a7c7
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-04-19 23:58:03 +0200

    add tag mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -10,7 +10,12 @@ module Types
     field :create_user, resolver: Mutations::UserMutations::CreateUser, null: false, authenticate: true
     field :edit_user, resolver: Mutations::UserMutations::EditUser, null: false, authenticate: true
     field :create_note, resolver: Mutations::NoteMutations::CreateNote, null: false, authenticate: true
+
     field :edit_note, resolver: Mutations::NoteMutations::EditNote, null: false, authenticate: true
     field :destroy_note, resolver: Mutations::NoteMutations::DestroyNote, null: false, authenticate: true
+
+    field :create_tag, resolver: Mutations::TagMutations::CreateTag, null: false, authenticate: true
+    field :edit_tag, resolver: Mutations::TagMutations::EditTag, null: false, authenticate: true
+    field :destroy_tag, resolver: Mutations::TagMutations::DestroyTag, null: false, authenticate: true
   end
 end
