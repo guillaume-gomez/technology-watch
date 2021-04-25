81b7cf4732c00ca3f15bc42a897604dfbaba4afc
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-04-26 00:10:31 +0200

    add relation between note_tag and note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,8 @@
 class Note < ApplicationRecord
   belongs_to :user
+  has_many :note_tags
+  has_many :tags, through: :note_tags
+
   validates :name, presence: true
   validates :link, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_222235) do
+ActiveRecord::Schema.define(version: 2021_04_24_222203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "note_tags", force: :cascade do |t|
+    t.bigint "note_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_note_tags_on_note_id"
+    t.index ["tag_id"], name: "index_note_tags_on_tag_id"
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string "name"
