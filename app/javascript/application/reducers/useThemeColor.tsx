11bf658ca46f3ae2ea3bae6a597fec9cc9d26d93
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-05-19 13:03:07 +0200

    set light as default

--- app/javascript/application/reducers/useThemeColor.tsx
+++ app/javascript/application/reducers/useThemeColor.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { createContainer } from "unstated-next";
 // simple reducer to store theme
-function useThemeMode(initialState : "dark"| "light" = "dark") {
+function useThemeMode(initialState : "dark"| "light" = "light") {
   let [themeMode, setThemeColor] = useState<"dark"| "light">(initialState)
   let toggleColor = () => setThemeColor(themeMode === "dark" ? "light": "dark")
 
