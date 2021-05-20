ab31d74a0adf3cfed7b115a32a36b474db3b6825
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-05-20 23:50:24 +0200

    fix seed to reuse it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+[
+  User,
+  Tag,
+  Note,
+].each(&:delete_all)
+
+
+
 john = User.create!(
   email: "john.doe@example.com",
   name: "john Doe",
