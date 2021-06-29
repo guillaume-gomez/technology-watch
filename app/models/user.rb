7ad6527f57dcd39ffdac4f8618f2739efa8ee27f
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-06-29 18:40:10 +0200

    generate default tag after user signed up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,:confirmable
 
+  DEFAULT_TAGS = %w(awesome interesting fun).freeze
+  after_create :create_default_tag
+
   has_many :notes, dependent: :destroy
   has_many :tags, dependent: :destroy
+
+  def create_default_tag
+    DEFAULT_TAGS.each do |tag_name|
+      tags.create(name: tag_name, color: "##{Random.bytes(3).unpack1('H*')}")
+    end
+  end
 end
