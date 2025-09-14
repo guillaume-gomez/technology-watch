6e57fba6f89fbdbf3561d70cb5c22b5aca4b432d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2025-09-14 19:50:03 +0200

    create uid for the user + add letter opener

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,:confirmable
 
   DEFAULT_TAGS = %w(awesome interesting fun).freeze
+  before_save :fill_uid
   after_create :create_default_tag
 
 
@@ -15,5 +16,10 @@ class User < ApplicationRecord
     DEFAULT_TAGS.each do |tag_name|
       tags.create(name: tag_name, color: "##{Random.bytes(3).unpack1('H*')}")
     end
+  end
+
+  def fill_uid
+    key = "#{email}-#{provider}"
+    self.uid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, key)
   end
 end
